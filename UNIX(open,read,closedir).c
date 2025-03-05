c1fd7da8a8bdaeb24c939fd8c4e0615f3a51bc7b
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2025-03-05 10:19:36 +0530

    Create UNIX(open,read,closedir).c

---
+++ UNIX(open,read,closedir).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+int main()
+{
+    char buff[100];
+    DIR *dirp;
+    struct dirent *dptr;
+    printf("\n\nEnter directory name: ");
+    scanf("%s", buff);
+    dirp = opendir(buff);
+    if (dirp == NULL) 
+    {
+        printf("The given directory does not exist.\n");
+        exit(1);
+    }
+    printf("Contents of directory %s:\n", buff);
+    while ((dptr = readdir(dirp)) != NULL)
+    {
+        printf("%s\n", dptr->d_name);
+    }
+    closedir(dirp);
+    return 0;
+}
