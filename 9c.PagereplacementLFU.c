c8fe2859d0e048a132a3dd2987df0cdc4ebfd463
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2025-03-12 11:50:03 +0530

    Update 9c.PagereplacementLFU.c

--- 9c.PagereplacementLFU.c
+++ 9c.PagereplacementLFU.c
@@ -1,82 +1,67 @@
-#include<stdio.h> main()
+#include <stdio.h>
 
-{
+ int main() {
+    int i, j, k, min, rs[25], m[10], count[10], flag[25], n, f, pf = 0, next = 1;
 
+    printf("Enter the length of reference string -- ");
+    scanf("%d", &n);
 
+    printf("Enter the reference string -- ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &rs[i]);
+        flag[i] = 0;
+    }
 
-intrs[50], i, j, k, m, f, cntr[20], a[20], min, pf=0; printf("\nEnter number of page references -- "); scanf("%d",&m);
+    printf("Enter the number of frames -- ");
+    scanf("%d", &f);
 
-printf("\nEnter the reference string -- "); for(i=0;i<m;i++)
+    for (i = 0; i < f; i++) {
+        count[i] = 0;
+        m[i] = -1;
+    }
 
-scanf("%d",&rs[i]);
+    printf("\nThe Page Replacement process is -- \n");
 
+    for (i = 0; i < n; i++) {
 
+        for (j = 0; j < f; j++) {
+            if (m[j] == rs[i]) {
+                flag[i] = 1;
+                count[j] = next;
+                next++;
+            }
+        }
 
-printf("\nEnter the available no. of frames -- "); scanf("%d",&f);
+        if (flag[i] == 0) {
+            if (i < f) {
+                m[i] = rs[i];
+                count[i] = next;
+                next++;
+            } else {
+                min = 0;
+                for (j = 1; j < f; j++) {
+                    if (count[min] > count[j]) {
+                        min = j;
+                    }
+                }
+                m[min] = rs[i];
+                count[min] = next;
+                next++;
+            }
+            pf++;
+        }
 
-for(i=0;i<f;i++)
+        for (j = 0; j < f; j++) {
+            printf("%d\t", m[j]);
+        }
 
+        if (flag[i] == 0) {
+            printf("PF No. -- %d", pf);
+        }
+        printf("\n");
+    }
 
+    printf("\nThe number of page faults using LRU are %d", pf);
 
-{
-
-
-
-cntr[i]=0; a[i]=-1;
-
+    getchar(); 
 }
-
-
-
-printf(―\nThe Page Replacement Process is – \n―); for(i=0;i<m;i++)
-
-{
-
-
-
-for(j=0;j<f;j++) if(rs[i]==a[j])
-
- 
-
-{
-
-
-
-cntr[j]++; break;
-
-}
-
-
-
-if(j==f)
-
-
-
-{
-
-
-
-min = 0; for(k=1;k<f;k++) if(cntr[k]<cntr[min]) min=k;
-
-a[min]=rs[i]; cntr[min]=1; pf++;
-
-}
-
-
-
-printf("\n"); for(j=0;j<f;j++) printf("\t%d",a[j]); if(j==f)
-
-printf(―\tPF No. %d‖,pf);
-
-
-
-}
-
-
-
-printf("\n\n Total number of page faults -- %d",pf);
-
-
-
-}
-
